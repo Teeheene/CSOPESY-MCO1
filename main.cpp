391af4ff6b05871eebf979e0d5df5ceaf6bf7f21
Teeheene <diane.panganiban1010@gmail.com>
2025-09-16 04:41:30 +0800

    chore: update display for main

--- main.cpp
+++ main.cpp
@@ -9,30 +9,26 @@
 
 int main() 
 {
-
 	bool running = true;
 
 	fetchDisplay();
 	while(running) {
 		std::vector<std::string> tokens = tokenizeInput( getInput() );
+		std::string command = tokens[0];
 		
-		if(tokens[0] == "neofetch") {
-			printLetters("dab");
+		if(command == "neofetch") {
+			printLetters("opesy");
 			fetchDisplay();
 		}
-		if(tokens[0] == "help") {
+		else if(command == "help") {
+			printLetters("help");
 			fetchHelpDisplay();
 		}
-		if(tokens[0] == "exit") {
+		else if(command == "exit") {
 			std::cout << "Terminating interpreter..." << std::endl;
 			running = false;
 		}
 	}
-
-	/*
-	std::cout << "Allowed characters: A-Z (automatically converted to uppercase), 0-9, spacebar" << std::endl;
-	printLetters("01234 abcd 56789");
-	*/
 
 	return 0;
 }
