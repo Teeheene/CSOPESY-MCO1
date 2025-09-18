0a5152e43e31cf362aea8e4842e739784c93db19
KenzoDevC <150371501+KenzoDevC@users.noreply.github.com>
2025-09-18 20:08:28 +0800

    keyboard handler and set_text command

--- main.cpp
+++ main.cpp
@@ -1,18 +1,15 @@
 #include <iostream>
-#include <thread> 
-#include <mutex> 
+#include <thread>
+#include <mutex>
 #include <atomic>
 #include <chrono>
 #include <vector>
 #include <string>
 #include <cctype>
-<<<<<<< HEAD
 #include <mutex>
 #include <queue>
-=======
 
-//headers
->>>>>>> 13f6c364a2a5cc358206e7c6e818b04ede655f0b
+// headers
 #include "ascii.h"
 #include "display.h"
 #include "logic.h"
@@ -24,87 +21,102 @@ std::mutex queueMutex;
 
 std::string marqueeText;
 
-void keyboardHandler() {
-	std::string command;
-	while (isRunning) {
-		std::getline(std::cin, command);
-		if (!command.empty()) {
-			std::unique_lock<std::mutex> lock(queueMutex);
-			commandQueue.push(command);
-			lock.unlock();
-		}
-	}
-}
-
-void marqueeLogic(int display_width) {
-	while(isRunning) {
-		std::this_thread::sleep_for(std::chrono::milliseconds(200));
-	}
-}
-
-void displayHandler() {
-	const int refreshRate = 50;
-	while(isRunning) {
-		std::this_thread::sleep_for(std::chrono::milliseconds(refreshRate));
-	}
-}
-
-int main() 
+void keyboardHandler()
 {
-	//these threads are already running
-	std::thread keyboard_handler_thread(keyboardHandler);
-	std::thread marquee_logic_thread(marqueeLogic,50);
-	std::thread display_handler_thread(displayHandler);
-		
-	bool running = true;
-	fetchDisplay();
-    while (isRunning) {
+    std::string command;
+    while (isRunning)
+    {
+        std::getline(std::cin, command);
+        if (!command.empty())
+        {
+            std::unique_lock<std::mutex> lock(queueMutex);
+            commandQueue.push(command);
+            lock.unlock();
+        }
+    }
+}
+
+void marqueeLogic(int display_width)
+{
+    while (isRunning)
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(200));
+    }
+}
+
+void displayHandler()
+{
+    const int refreshRate = 50;
+    while (isRunning)
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(refreshRate));
+    }
+}
+
+int main()
+{
+    // these threads are already running
+    std::thread keyboard_handler_thread(keyboardHandler);
+    std::thread marquee_logic_thread(marqueeLogic, 50);
+    std::thread display_handler_thread(displayHandler);
+
+    bool running = true;
+    fetchDisplay();
+    while (isRunning)
+    {
         std::string command;
         std::vector<std::string> tokens;
 
         {
             std::unique_lock<std::mutex> lock(queueMutex);
-            if (!commandQueue.empty()) {
+            if (!commandQueue.empty())
+            {
                 command = commandQueue.front();
                 tokens = tokenizeInput(command);
                 commandQueue.pop();
             }
         }
 
-        if (!command.empty()) {
-            if (tokens[0] == "exit") {
+        if (!command.empty())
+        {
+            if (tokens[0] == "exit")
+            {
                 std::cout << "Terminating interpreter..." << std::endl;
                 isRunning = false;
             }
-            else if (tokens[0] == "help") {
+            else if (tokens[0] == "help")
+            {
                 printLetters("help");
                 fetchHelpDisplay();
             }
-            else if (tokens[0] == "neofetch") {
+            else if (tokens[0] == "neofetch")
+            {
                 printLetters("opesy");
                 fetchDisplay();
             }
-            else if (tokens[0] == "set_text") {
+            else if (tokens[0] == "set_text")
+            {
                 int i = 1;
                 marqueeText.clear();
-                while (i < tokens.size()) { 
+                while (i < tokens.size())
+                {
                     marqueeText += tokens[i] + " ";
                     i++;
                 }
 
-                if (!marqueeText.empty()) {
+                if (!marqueeText.empty())
+                {
                     marqueeText.pop_back();
                 }
                 printLetters(marqueeText);
             }
-            if (isRunning) {
+            if (isRunning)
+            {
                 std::cout << "\nCommand> " << std::flush;
             }
-
         }
 
         std::this_thread::sleep_for(std::chrono::milliseconds(10));
     }
-	return 0;
+    return 0;
 }
-
